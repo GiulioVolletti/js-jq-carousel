904b8768effc2dd0662f9602310a62945acc7b2f
Giulio <giuliovolletti@hotmail.com>
2020-11-24 16:04:40 +0100

    commit2

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(
       function (){
         console.log(next);
         goToRigth();
+
       }
     )
 
@@ -16,6 +17,7 @@ $(document).ready(
       function (){
         console.log(prev);
         goToLeft();
+
       }
     )
 
@@ -23,14 +25,31 @@ $(document).ready(
 )
 
 function goToRigth(){
-  $(".images img.active").removeClass("active").next().addClass("active");
 
-  $(".nav i.active").removeClass("active").next().addClass("active");
+  // console.log($(".images img.active").hasClass("last"));
+  if ($(".images img.active").hasClass("last") == true) {
+    $(".nav i.active").removeClass("active");
+    $(".images img.active").removeClass("active");
+    $(".images img.first").addClass("active");
+    $(".nav i.first").addClass("active");
+  } else {
+    $(".images img.active").removeClass("active").next().addClass("active");
+
+    $(".nav i.active").removeClass("active").next().addClass("active");
+  }
 
 }
 
 function goToLeft(){
-  $(".images img.active").removeClass("active").prev().addClass("active");
+  if ($(".images img.active").hasClass("first") == true) {
+    $(".nav i.active").removeClass("active");
+    $(".images img.active").removeClass("active");
+    $(".images img.last").addClass("active");
+    $(".nav i.last").addClass("active");
+  } else {
+    $(".images img.active").removeClass("active").prev().addClass("active");
 
-  $(".nav i.active").removeClass("active").prev().addClass("active");
+    $(".nav i.active").removeClass("active").prev().addClass("active");
+  }
+
 }
