b88e4e46c55af782be50840424d85b3914206f41
Giulio <giuliovolletti@hotmail.com>
2020-12-09 09:08:35 +0100

    push

--- js/main.js
+++ js/main.js
@@ -28,12 +28,42 @@ $(document).ready(
         } else if (event.which == 37) {
           goToLeft();
         }
-      console.log(event.which);
+      // console.log(event.which);
     })
+    $(".nav i").click(
+      function (){
+        $(".images img.active").removeClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+        var circleIndex = $(this).index();
 
+        $(".images > img").eq(circleIndex).addClass("active");
+        var here = $(this);
+        console.log(here);
+        console.log($(".nav i"));
+
+        // for (var i = 0; i < $(".nav i").length; i++) {
+        //   if ($(".nav i")[i] == here[0]) {
+        //         // $(".images > img").index().eq([i]).addClass("active");
+        //     // $(".images img:nth-child(" + [i] + 1 + ")").addClass("active");
+        //
+        //     // console.log( $(".images img:nth-child(" + [i] + ")").addClass(" active"));
+        //
+        //
+        //   }
+        //   // console.log($(".nav i")[i] == here[0]);
+        //   // $(".images img")[i].addClass("active");
+        // }
+      }
+    )
   }
 )
 
+// var arrayIcon = $(".nav i");
+// for (var i = 0; i < arrayIcon.length; i++) {
+//
+// }
+// -------------------- function ---------- //
 function goToRigth(){
 
   // console.log($(".images img.active").hasClass("last"));
